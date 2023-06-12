594a4498291822728d8d92a749a29483840547ed
Sarahx35 <110346175+Sarahx35@users.noreply.github.com>
2023-06-12 16:16:48 +0100

    Added Blog Overview page
    Added Blog overview page and added some JS to update footer date automatically

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+
+// Opening and closing mobile hamburger menu bar in mobile view 
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
+
+// Updating the footer year automatically
+const copyrightName = "Acem Inc.";
+const copyrightDate = new Date();
+document.write("&copy; " + copyrightName + " " + copyrightDate.getFullYear());
+
